6bdfcf124c4a6df2d89d5d805dbccf098684121b
yaswant-singhi <ysinghi@deloitte.com>
2024-11-06 13:31:19 +0530

    groq and workflow

--- pdf_qa_ai/backend/main.py
+++ pdf_qa_ai/backend/main.py
@@ -1,5 +1,7 @@
 import pdfplumber
+from groq import Groq
 
+client = Groq(api_key="your_api_key")
 
 def read_pdf_document(pdf_file):
     text = ""
@@ -7,3 +9,17 @@ def read_pdf_document(pdf_file):
         for p in pdf.pages:
             text += p.extract_text() or ""
     return text
+
+def generate_response(extracted_data, query):
+    prompt = f"extracted_data:{extracted_data}\nQuestion: {query}\nAnswer:"
+    chat_completion = client.chat.completions.create(
+        messages=[
+            {
+                "role": "user",
+                "content": prompt,
+            }
+        ],
+        model="llama3-8b-8192",
+    )
+    response = chat_completion.choices[0].message.content
+    return response
--- pdf_qa_ai/frontend/app.py
+++ pdf_qa_ai/frontend/app.py
@@ -1,5 +1,6 @@
 import streamlit as st
-from pdf_qa_ai.backend.main import read_pdf_document
+import gc
+from pdf_qa_ai.backend.main import read_pdf_document,generate_response
 
 
 # Set page config
@@ -30,3 +31,23 @@ if is_file_uploaded:
     extracted_data = read_pdf_document(is_file_uploaded)
     st.markdown("")
     st.text_area("Extracted data", extracted_data, height=250)
+
+    # Query Section
+    query = st.text_input("Enter your query here")
+
+    if st.button("Ask Question"):
+        if query:
+            with st.spinner("Answering the question..."):
+                response = generate_response(extracted_data,query)
+                st.write("Response: ", response)
+
+            # Clear memory
+            gc.collect()
+        else:
+            st.warning("Please enter the query to get the response")    
+
+# sidebar Footer
+st.sidebar.markdown("### About")
+st.sidebar.info("Developed using Streamlit and Groq API.")
+st.sidebar.markdown("---")
+st.sidebar.write("For more information, visit [Groq](https://www.groq.com) and [Streamlit](https://streamlit.io).")
