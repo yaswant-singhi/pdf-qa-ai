58fdef3df4fe022545e237b4469d90e1c3baa1fa
yaswant-singhi <ysinghi@deloitte.com>
2024-11-06 13:41:38 +0530

    update setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,26 @@
 from setuptools import setup, find_packages
 from pdf_qa_ai import __version__
 
+
+PACKAGE_REQUIREMENTS = [
+    "pdfplumber",
+    "groq",
+    "streamlit",
+    "gc",
+]
+
+DEV_REQUIREMENTS = [
+    "black"
+]
+
 setup(
     name="",
     packages=find_packages(exclude=["tests"]),
     setup_requires=["setuptools", "wheel"],
+    install_requires = PACKAGE_REQUIREMENTS,
+    extras_require={
+        "dev": DEV_REQUIREMENTS,
+    },
     version=__version__,
     description="PDF Q&A AI",
     author="",
